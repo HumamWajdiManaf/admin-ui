323e3f03f1ba689adc71726bd3b46b46f5d41175
HumamWajdiManaf <humammanaf10@gmail.com>
2025-11-04 05:23:44 +0700

    React Hooks (useState & useEffect)

--- src/Exercise.jsx
+++ src/Exercise.jsx
@@ -1,34 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import UserCard from "./UserCard";
+import { getUsers } from "./Services";
 
 function Exercise() {
-  return (
-    <>
-      <div className="min-h-screen bg-gray-100 p-6">
-        <h1 className="text-3xl font-bold text-center mb-6 text-blue-700">
-          User Cards
-        </h1>
-        <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-6 max-w-5xl mx-auto">
-          <UserCard 
-          name="Humam"
-          email="humammanaf10@gmail.com"
-          street="Jl Abimanyu No 5"
-          city="Semarang Tengah"/>
-          
-          <UserCard 
-          name="Wajdi"
-          email="wajdimanaf10@gmail.com"
-          street="Jl Abimanyu No 4"
-          city="Semarang Utara"/>
+  const [users, setUsers] = useState([]);
 
-          <UserCard 
-          name="Manaf"
-          email="manafhumam10@gmail.com"
-          street="Jl Abimanyu No 7"
-          city="Semarang Timur"/>
-        </div>
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await getUsers(); // Calls getUsers to fetch data
+        setUsers(data); // Updates state with the fetched users
+      } catch (error) {
+        console.error("[Component] Gagal menampilkan data:", error.message);
+      }
+    };
+    fetchData();
+  }, []); // Empty dependency array ensures this runs only once on component mount
+
+  return (
+    <div className="min-h-screen bg-gray-100 p-6">
+      <h1 className="text-3xl font-bold text-center mb-6 text-blue-700">
+        User Cards
+      </h1>
+      <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-6 max-w-5xl mx-auto">
+        {users.map((user) => (
+          <UserCard key={user.email} {...user} />
+        ))}
       </div>
-    </>
+    </div>
   );
 }
 
